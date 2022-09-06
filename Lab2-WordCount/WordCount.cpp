2a70a8b9afd38f60d2b072f237b346ab1c62cf66
Millhouse32 <nlm82@uakron.edu>
2022-09-06 12:30:57 -0400

    Add Makefile

---
+++ Lab2-WordCount/WordCount.cpp
@@ -0,0 +1,5 @@
+#include "WordCount.hpp"
+
+int main(){
+    cout << "Hello world" << endl;
+}
--- Lab2-WordCount/WordCount.hpp
+++ Lab2-WordCount/WordCount.hpp
@@ -2,6 +2,8 @@
 // Author: Nicholas Miller
 #include <stdio.h>
 #include <string>
+#include <iostream>
+
 using namespace std;
 
 class WordOccurence {
