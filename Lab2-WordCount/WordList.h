9e9614ae35364fafe57d5d79ce1e24ec5d539b44
Millhouse32 <nlm82@uakron.edu>
2022-09-06 13:30:02 -0400

    Udpate header files

--- Lab2-WordCount/WordList.h
+++ Lab2-WordCount/WordList.h
@@ -1,6 +1,5 @@
 // Word Count Header file 
 // Author: Nicholas Miller
-#include <stdio.h>
 #include <string>
 #include <iostream>
 #include "WordOccurrence.h"
@@ -8,14 +7,39 @@
 using namespace std;
 
 class WordList {
-    public:
+public:
     // add copy constructor, destructor, and overload assignment
+
+    WordList(const WordList& org); // copy constructor
+    ~WordList(); // destructor
+    WordList& operator = (const WordList& rhs); // overloaded assignment
+
+    WordList();
+    WordList(const string &word);
+
+
     void addWord(const string &);
     void printList();
-    private:
+private:
     // a dynamically allocated array of WordOccurrences
     // may or may not be stored
-    WordOccurrence *wordArray ;
+    WordOccurrence *wordArray_ ;
 
-    int size ;
+    int size_ ;
 };
+
+WordList::WordList(const WordList& org){
+    size_ = org.size_;
+
+    wordArray_ = new WordOccurrence[org.size_];
+    // copy each element of org.wordArray_ to this.wordArray_
+    for (int i = 0; i < size_; i++) {
+        wordArray_[i] = WordOccurrence(org.wordArray_[i].getWord(), org.wordArray_[i].getNum());
+    }
+}
+
+WordList::~WordList() {
+    // executed when wordList object goes out of scope (program ends)
+    delete[] wordArray_;
+}
+
