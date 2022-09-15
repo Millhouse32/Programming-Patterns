4ab48868f35cf1fe4b00acb5686ba91e520abadc
Millhouse32 <nlm82@uakron.edu>
2022-09-15 16:32:03 -0400

    Add functionality to rosterObject.cpp

--- Lab4-Roster-Without-Conflicts/rosterObject.cpp
+++ Lab4-Roster-Without-Conflicts/rosterObject.cpp
@@ -60,6 +60,78 @@ void readRoster(std::list<Student>& roster, std::string filename);
 void printRoster(const std::list<Student> & roster);
 
 int main(int argc, char* argv[]) {
-    
+
+    if (argc <= 1) {
+        std::cout << "useage: " << argv[0]
+        << " list of courses, dropouts last" << std::endl;
+        exit(1);
+    }
+
+    // vector of courses of students
+    std::vector <std::list<Student>> courseStudents;
+
+    for (int i = 1; i < argc-1; ++i) {
+        std::list<Student> roster;
+        readRoster(roster, argv[i]);
+        std::cout << "\n\n" << argv[i] << "\n";
+        printRoster(roster);
+        courseStudents.push_back(move(roster));
+    }
+
+    // read dropouts
+    std::list<Student> dropouts;
+    readRoster(dropouts, argv[argc-1]);
+    std::cout << "\n\n dropouts \n";
+    printRoster(dropouts);
+
+    // master list of students
+    std::list<Student> masterList;
+
+    std::cout << "\n\n Master list unsorted: \n";
+    printRoster(masterList);
+
+    // Sort master list
+    masterList.sort();
+
+    // print sorted
+    std::cout << "\n\n Master list sorted: \n";
+    printRoster(masterList);
+
+    // get rid of repeats
+    masterList.unique();
+    std::cout << "\n\n Master list without repeats \n";
+    printRoster(masterList);
+
+    // remove droputs
+    for (const auto& str : dropouts) {
+        masterList.remove(str);
+    }
+    std::cout << "\n\n Master list without dropouts: \n";
+    printRoster(masterList);
+
     return 0;
+}
+
+void readRoster(std::list<Student>& roster, std::string filename) {
+    std::ifstream course(filename);
+    int i = 0;
+    std::string currentCourse;
+    while (filename.at(i) != ".") {
+        currentCourse += filename.at(i);
+        ++i;
+    }
+
+    std::string first;
+    std::string last;
+
+    while (course >> first >> last) {
+        roster.push_back(Student(first, last, currentCourse));
+    }
+    course.close();
+}
+
+// printing list
+void printRoster(const std::list<Student>& roster) {
+    for (const auto& student : roster)
+        std::cout << student.print() << std::endl;
 }
